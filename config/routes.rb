b225562be023ff5c4997dedc57e796d63e67ac41
hiromi-mitsuoka <hiro.m.19981217@ezweb.ne.jp>
2021-10-30 13:00:58 +0900

    No data but good layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'categories/index'
   root "articles#index"
-  resources :articles
+  resources :articles, only: [:index]
+  resources :categories, only: [:index]
 
   # 後ほどファイル分割
   namespace :api do
     namespace :v1 do
-      resources :articles
+      resources :articles, only: [:index]
     end
   end
 end
