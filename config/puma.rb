b5a3d77118088bb9ed4ee200a343e30c33cf14e8
hiromi-mitsuoka <hiro.m.19981217@ezweb.ne.jp>
2021-11-30 14:36:03 +0900

    Remove worker

--- config/puma.rb
+++ config/puma.rb
@@ -55,8 +55,6 @@ threads threads_count, threads_count
 environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
 
-worker_timeout 30
-
 app_root = File.expand_path("../..", __FILE__)
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
 
