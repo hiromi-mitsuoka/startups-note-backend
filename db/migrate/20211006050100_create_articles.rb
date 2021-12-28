28b7428975a769636e7470383345e5f5da425d81
hiromi-mitsuoka <hiro.m.19981217@ezweb.ne.jp>
2021-12-28 19:18:25 +0900

    Change article.categories from string to text

--- db/migrate/20211006050100_create_articles.rb
+++ db/migrate/20211006050100_create_articles.rb
@@ -7,7 +7,10 @@ class CreateArticles < ActiveRecord::Migration[6.1]
       t.string :image
       t.date :published
       # TechCrunch用に512バイトに変更
-      t.string :categories, limit: 512
+      # t.string :categories, limit: 512
+
+      # To deal with error(Mysql2::Error: Data too long for column 'categories' at row 24)
+      t.text :categories
 
       t.timestamps
     end
