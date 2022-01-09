917c436a2bde5856c2aed3c5eef4bd734bf64575
hiromi-mitsuoka <hiro.m.19981217@ezweb.ne.jp>
2022-01-09 17:14:32 +0900

    Create user with firebase auth, but not using auth_providers_controller

--- app/models/auth_provider.rb
+++ app/models/auth_provider.rb
@@ -1,2 +1,3 @@
 class AuthProvider < ApplicationRecord
+  belongs_to :user, foreign_key: "user_id"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :articles, only: [:index]
       resources :categories, only: [:index]
+      resources :users, only: [:create]
     end
   end
 end
