5ba24d5d8123318ee50a18ff750f535d22598d1c
hiromi-mitsuoka <hiro.m.19981217@ezweb.ne.jp>
2022-01-11 10:04:44 +0900

    Add comment.destroy

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,10 +1,13 @@
 class Api::V1::CommentsController < Api::ApplicationController
+  before_action :set_comment, only: %i[destroy]
+
   # def new
   # end
 
   def create
     user_id = User.find_by!(uid: comment_params[:uid]).id
     comment = Comment.new(comment_params)
+    # Since user_id cannot be posted from the frontend side, it is handled on the backend side.
     comment.user_id = user_id
     comment.save!
     render json: comment
@@ -17,10 +20,16 @@ class Api::V1::CommentsController < Api::ApplicationController
   # end
 
   def destroy
-
+    if @comment.destroy
+      render json: nil
+    end
   end
 
   private
+
+  def set_comment
+    @comment = Comment.find_by(id: params[:id])
+  end
 
   def comment_params
     params.permit(:uid, :article_id, :text)
