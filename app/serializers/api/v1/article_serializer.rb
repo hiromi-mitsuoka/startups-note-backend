87a59753d738bc887100974b29f39a918d4ff4eb
hiromi-mitsuoka <hiro.m.19981217@ezweb.ne.jp>
2022-01-16 14:30:10 +0900

    Fix Api

--- app/serializers/api/v1/article_serializer.rb
+++ app/serializers/api/v1/article_serializer.rb
@@ -1,8 +1,8 @@
-class Ap1::V1::ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :url, :published, :medium_name
-  # , :image, :categories, :deleted_at
+# class Api::V1::ArticleSerializer < ActiveModel::Serializer
+#   attributes :id, :title, :url, :published, :medium_name
+#   # , :image, :categories, :deleted_at
 
-  def medium_name
-    Medium.find(object.medium_id).name ||= "" # guard nil
-  end
-end
+#   def medium_name
+#     Medium.find(object.medium_id).name ||= "" # guard nil
+#   end
+# end
